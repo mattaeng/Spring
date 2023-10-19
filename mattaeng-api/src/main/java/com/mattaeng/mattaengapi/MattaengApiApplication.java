6c06ed47b16ffcaa1d5cdc2fc570d4e8e166f8fd
소영섭 <9412sys@gmail.com>
2023-10-19 23:48:23 +0900

    feat: git ignore setting

---
+++ mattaeng-api/src/main/java/com/mattaeng/mattaengapi/MattaengApiApplication.java
@@ -0,0 +1,13 @@
+package com.mattaeng.mattaengapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattaengApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattaengApiApplication.class, args);
+	}
+
+}
---
+++ mattaeng-api/src/test/java/com/mattaeng/mattaengapi/MattaengApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattaeng.mattaengapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattaengApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

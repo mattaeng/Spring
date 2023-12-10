ecb047c0ceb0b57a19f825be237b65827a7fe103
So YoungSeob <9412sys@gmail.com>
2023-12-10 18:59:38 +0900

    feat: SwaggerConfig추가

--- src/main/java/common/config/SwaggerConfig.java
+++ src/main/java/common/config/SwaggerConfig.java
@@ -1,2 +1,47 @@
-package common.config;public class SwaggerConfig {
+package common.config;
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "authorization";
+
+    @Bean
+    public OpenAPI swaggerApi() {
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                    .name(SECURITY_SCHEME_NAME)
+                    .type(Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .info(new Info()
+                .title("Mattaeng Test")
+                .description("Mattaeng Swagger Test")
+                .version("1.0,0"));
+    }
+    @Bean
+    public OperationCustomizer globalHeader() {
+        return (operation, handlerMethod) -> {
+            operation.addParametersItem(new Parameter()
+                .in(ParameterIn.HEADER.toString())
+                .schema(new StringSchema().name("Refresh-Token"))
+                .name("Refresh-Token"));
+            return operation;
+        };
+    }
 }
+

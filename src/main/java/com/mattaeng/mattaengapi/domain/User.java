59ca093ef95038b60ea9ca22fdf2316c3914cf85
DaehoSon <thseogh135@gmail.com>
2023-12-11 01:50:10 +0900

    Feat: User 엔티티 추가

---
+++ src/main/java/com/mattaeng/mattaengapi/domain/User.java
@@ -0,0 +1,45 @@
+package com.mattaeng.mattaengapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

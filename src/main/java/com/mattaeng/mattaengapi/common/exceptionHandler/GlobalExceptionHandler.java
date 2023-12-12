5324837bbaae8cf72ef3239f60c4a28aeb0d9360
DaehoSon <thseogh135@gmail.com>
2023-12-12 22:36:46 +0900

    Update: ExceptionResponse를 반환하는 클래스 생성
    - ExceptionResponse(ErrorCodeIfs errorCode, String description)
    - build 메서드를 통해 ExceptionResponse 반환

--- src/main/java/com/mattaeng/mattaengapi/common/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/mattaeng/mattaengapi/common/exceptionHandler/GlobalExceptionHandler.java
@@ -1,22 +1,24 @@
 package com.mattaeng.mattaengapi.common.exceptionHandler;
 
-import com.mattaeng.mattaengapi.common.api.Api;
-import com.mattaeng.mattaengapi.common.error.ErrorCode;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.mattaeng.mattaengapi.common.api.Api;
+import com.mattaeng.mattaengapi.common.error.ErrorCode;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RestControllerAdvice
 @Order(value = Integer.MAX_VALUE)
 public class GlobalExceptionHandler {
-    @ExceptionHandler({Exception.class})
-    public ResponseEntity<Api<Object>> exception(Exception exception){
-        log.error("GlobalExceptionHandler : {}", exception.getLocalizedMessage());
-        return ResponseEntity
-            .status(500)
-            .body(Api.error(ErrorCode.SERVER_ERROR, exception.getMessage()));
-    }
+
+	@ExceptionHandler({Exception.class})
+	public ResponseEntity<Api<Object>> exception(Exception exception) {
+		return new ExceptionResponseBuilder(ErrorCode.SERVER_ERROR, exception.getMessage())
+			.build();
+	}
+
 }

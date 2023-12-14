09089e03042e705cd60ddb7057d06bab1311aa17
DaehoSon <thseogh135@gmail.com>
2023-12-14 21:25:08 +0900

    Update: 이미 존재하는 유저의 경우 예외 처리
    - userId와 phoneNumber로 판단

--- src/main/java/com/mattaeng/mattaengapi/repository/UserRepository.java
+++ src/main/java/com/mattaeng/mattaengapi/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.mattaeng.mattaengapi.repository;
 
-import com.mattaeng.mattaengapi.domain.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.mattaeng.mattaengapi.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Boolean existsByUsername(String username);
+	Boolean existsByUserIdOrPhoneNumber(String userId, String phoneNumber);
 
-    Optional<User> findByUsername(String username);
+	Optional<User> getByUserId(String userId);
 }
--- src/main/java/com/mattaeng/mattaengapi/service/UserService.java
+++ src/main/java/com/mattaeng/mattaengapi/service/UserService.java
@@ -1,25 +1,29 @@
 package com.mattaeng.mattaengapi.service;
 
+import org.springframework.stereotype.Service;
+
 import com.mattaeng.mattaengapi.common.error.CommonErrorCode;
 import com.mattaeng.mattaengapi.common.exception.ApiException;
 import com.mattaeng.mattaengapi.domain.User;
 import com.mattaeng.mattaengapi.dto.user.CreateUserRequest;
 import com.mattaeng.mattaengapi.dto.user.CreateUserResponse;
 import com.mattaeng.mattaengapi.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public CreateUserResponse createUser(CreateUserRequest createUserRequest) {
-        if (userRepository.existsByUsername(createUserRequest.username())) {
-            throw new ApiException(CommonErrorCode.BAD_REQUEST, "이미 존재하는 유저입니다.");
-        }
-        User user = userRepository.save(createUserRequest.toEntity());
-        return user.toCreateUserResponse();
-    }
+	public CreateUserResponse createUser(CreateUserRequest request) {
+		if (userRepository.existsByUserIdOrPhoneNumber(request.userId(), request.phoneNumber())) {
+			throw new ApiException(CommonErrorCode.BAD_REQUEST, "이미 존재하는 유저입니다.");
+		}
+		User user = userRepository.save(request.toEntity());
+		return user.toCreateUserResponse();
+	}
 }

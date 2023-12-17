66f41e728db439adca5aa895ce0791ef116f5499
DaehoSon <thseogh135@gmail.com>
2023-12-17 22:56:27 +0900

    Fix: User Repository의 JpaRepository의 ID 타입 수정. Long -> UUID
    - 기본키 전략을 Long에서 UUID로 변경했기 때문에 이에 맞게 수정함

--- src/main/java/com/mattaeng/mattaengapi/repository/UserRepository.java
+++ src/main/java/com/mattaeng/mattaengapi/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.mattaeng.mattaengapi.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mattaeng.mattaengapi.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
 	Boolean existsByUserIdOrPhoneNumber(String userId, String phoneNumber);
 

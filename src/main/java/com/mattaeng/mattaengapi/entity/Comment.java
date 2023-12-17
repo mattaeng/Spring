d61a104af30fb7ba28f8d02c78b5512fb9fad623
So YoungSeob <9412sys@gmail.com>
2023-12-18 07:26:49 +0900

    feat: Comment Entity 생성 및 속성 추가

--- src/main/java/com/mattaeng/mattaengapi/entity/Comment.java
+++ src/main/java/com/mattaeng/mattaengapi/entity/Comment.java
@@ -1,2 +1,44 @@
-package com.mattaeng.mattaengapi.entity;public class Comment {
+package com.mattaeng.mattaengapi.entity;
+
+import com.mattaeng.mattaengapi.common.auditing.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Entity
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String context;
+
+    @ManyToOne
+    @JoinColumn(name ="feed_id")
+    private Feed feed;
+
+    public Comment() {
+
+    }
+
+    private Comment(Long id, String context, Feed feed){
+        this.id = id;
+        this.context = context;
+        this.feed = feed;
+    }
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public void setFeed(Feed feed) {
+        this.feed = feed;
+    }
 }

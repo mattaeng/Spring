64c4da46366cf961b6d5fce39bbd631dc9ca5590
DaehoSon <thseogh135@gmail.com>
2023-12-19 23:10:51 +0900

    Feat: jwtAuthenticationFilter를 제외하는 uri 검사 추가

--- src/main/java/com/mattaeng/mattaengapi/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/mattaeng/mattaengapi/security/jwt/JwtAuthenticationFilter.java
@@ -15,13 +15,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+	private final String SWAGGER_ENDPOINT = "/swagger-ui";
+	private final String LOGIN_ENDPOINT = "/api/v1/login";
+	private final String SIGNUP_ENDPOINT = "/api/v1/users";
+
 	private final JwtProvider jwtProvider;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
 	) throws ServletException, IOException {
-		String jws = jwtProvider.extractJwsFromRequest(request);
-		jwtProvider.verifyJws(jws);
+		if (!isExcludeUri(request.getRequestURI())) {
+			String jws = jwtProvider.extractJwsFromRequest(request);
+			jwtProvider.verifyJws(jws);
+		}
 		filterChain.doFilter(request, response);
+	}
+
+	private Boolean isExcludeUri(String uri) {
+		return uri.startsWith(SWAGGER_ENDPOINT) ||
+			uri.equals(LOGIN_ENDPOINT) ||
+			uri.equals(SIGNUP_ENDPOINT);
 	}
 }

c056835a6a88c97e697055d9b4b4a03391927ad4
DaehoSon <thseogh135@gmail.com>
2023-12-21 23:36:54 +0900

    Refact: Request에 들어오는 password의 유효성을 검사하는 커스텀 어노테이션 추가
    [추가 사항]
    - Password
    : public `@`interface Password
    : Target(ElementType.FIELD) - 필드에만 적용
      Retention(RetentionPolicy.RUNTIME) - 어노테이션이 유지되는 시간이 런타임 시에도
      `@`Constraint(validatedBy = PasswordValidator.class) - 유효성 검사 클래스
      `@`Documented - JavaDoc 생성 시, 어노테이션에 대한 정보도 함께 생성
    : message - 유효하지 않을 경우 반환할 메시지
    : groups - 유효성 검증이 진행될 그룹. ex) user, admin
    : payload - 유효성 검증시에 전달할 메타 정보
    
    - PasswordValidator
    : class PasswordValidator implements ConstraintValidator<Password, String>
    : 패턴을 검사해서 boolean값 반환
    
    [변경 사항]
    - CreateUserRequest
    : `@`Password로 변경
    
    - UpdateUserPasswordRequest
    : `@`Password로 변경
    
    [참고 자료]
    - https://mangkyu.tistory.com/206

--- src/main/java/com/mattaeng/mattaengapi/dto/user/CreateUserRequest.java
+++ src/main/java/com/mattaeng/mattaengapi/dto/user/CreateUserRequest.java
@@ -1,5 +1,6 @@
 package com.mattaeng.mattaengapi.dto.user;
 
+import com.mattaeng.mattaengapi.common.annotations.Password.Password;
 import com.mattaeng.mattaengapi.domain.User;
 import com.mattaeng.mattaengapi.security.BCryptConfig;
 
@@ -13,11 +14,8 @@ public record CreateUserRequest(
 	@Email(message = "이메일 형식으로 입력해주세요")
 	String userId,
 
+	@Password
 	@NotBlank(message = "비밀번호를 입력해주세요")
-	@Pattern(
-		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{8,}$",
-		message = "잘못된 패스워드입니다. 공백을 제외한 소문자, 대문자, 특수문자, 숫자를 포함하여 8글자 이상 입력해주세요"
-	)
 	String password,
 
 	@NotBlank(message = "이름을 입력해주세요")
--- src/main/java/com/mattaeng/mattaengapi/dto/user/UpdateUserPasswordRequest.java
+++ src/main/java/com/mattaeng/mattaengapi/dto/user/UpdateUserPasswordRequest.java
@@ -1,21 +1,15 @@
 package com.mattaeng.mattaengapi.dto.user;
 
+import com.mattaeng.mattaengapi.common.annotations.Password.Password;
+
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 public record UpdateUserPasswordRequest(
-	@NotBlank(message = "기존 비밀번호를 입력해주세요")
-	@Pattern(
-		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{8,}$",
-		message = "잘못된 패스워드입니다. 공백을 제외한 소문자, 대문자, 특수문자, 숫자를 포함하여 8글자 이상 입력해주세요"
-	)
+	@Password
 	String oldPassword,
 
+	@Password
 	@NotBlank(message = "새로운 비밀번호를 입력해주세요")
-	@Pattern(
-		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*()]{8,}$",
-		message = "잘못된 패스워드입니다. 공백을 제외한 소문자, 대문자, 특수문자, 숫자를 포함하여 8글자 이상 입력해주세요"
-	)
 	String newPassword
 ) {
 }

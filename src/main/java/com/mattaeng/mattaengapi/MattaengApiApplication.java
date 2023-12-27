75d20b265a84523fd5afc2b719f6870296cf4ffc
So YoungSeob <9412sys@gmail.com>
2023-12-28 08:23:56 +0900

    feat: TimeZone(Asia/Seoul) 설정

--- src/main/java/com/mattaeng/mattaengapi/MattaengApiApplication.java
+++ src/main/java/com/mattaeng/mattaengapi/MattaengApiApplication.java
@@ -1,12 +1,21 @@
 package com.mattaeng.mattaengapi;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MattaengApiApplication {
+
+	@PostConstruct
+	void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MattaengApiApplication.class, args);

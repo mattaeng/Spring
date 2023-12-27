04608ebefd7231e59921dcb1a534336f94a0b3b1
So YoungSeob <9412sys@gmail.com>
2023-12-27 12:33:40 +0900

    fix: addHeart -> updateHeart, removeHeart -> deleteHeart 변경

--- src/main/java/com/mattaeng/mattaengapi/entity/Heart.java
+++ src/main/java/com/mattaeng/mattaengapi/entity/Heart.java
@@ -1,42 +1,56 @@
 package com.mattaeng.mattaengapi.entity;
 
 import com.mattaeng.mattaengapi.common.auditing.BaseTimeEntity;
-import com.mattaeng.mattaengapi.common.enums.PreferenceStatus;
-import com.mattaeng.mattaengapi.dto.preference.PostPreferenceRequest;
+import com.mattaeng.mattaengapi.common.enums.HeartStatus;
+import com.mattaeng.mattaengapi.dto.heart.HeartRequest;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Entity
 @Builder
-public class Preference extends BaseTimeEntity {
+public class Heart extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Enumerated(EnumType.STRING)
+	private HeartStatus heartStatus;
 
-    @Enumerated(EnumType.STRING)
-    private PreferenceStatus preferenceStatus;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "feed_id")
+	private Feed feed;
 
-    public Preference() {
+	public Heart() {
 
-    }
+	}
 
-    public Preference(Long id, PreferenceStatus preferenceStatus) {
-        this.id = id;
-        this.preferenceStatus = preferenceStatus;
-    }
+	public Heart(Long id, HeartStatus heartStatus, Feed feed) {
+		this.id = id;
+		this.heartStatus = heartStatus;
+		this.feed = feed;
+	}
 
-    // TODO: activate and inactivate 나눠서 로직 작성
-    public void updateLikeStatus(PostPreferenceRequest postPreferenceRequest){
-        if (postPreferenceRequest.preferenceStatus() != null){
-            this.preferenceStatus = PreferenceStatus.INACTIVATE;
-        }
-    }
+	// TODO: activate and inactivate 나눠서 로직 작성
+	public void updateHeart(HeartRequest heartRequest) {
+		if (heartRequest.heartStatus() != null) {
+			this.heartStatus = HeartStatus.ACTIVATIE;
+		}
+	}
+
+	public void deleteHeart(HeartRequest heartRequest) {
+		if (heartRequest.heartStatus() != null) {
+			this.heartStatus = HeartStatus.INACTIVATE;
+		}
+	}
 }
